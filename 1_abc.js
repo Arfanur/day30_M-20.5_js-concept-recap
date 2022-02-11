737c558c0884d233124cadc91e53b9f55607c406
Arfanur Rahman <arfanur03@gmail.com>
2022-02-11 20:50:16 +0600

    first commit

---
+++ 1_abc.js
@@ -0,0 +1,27 @@
+/* 
+core concepts
+variable (let, const) javascript var, let, const
+array, loop (while, for), conditionals, function
+
+function inchToFeet() {}
+
+leapYear
+odd even
+factorial (while, for)
+
+New home work
+
+1.
+celciusToFarenheit(celcius) {}
+
+2.
+farenheitToCelcius(farenheit) {}
+
+3.
+grade calculation
+hints: use if, else if, else
+
+4.
+simple interest(firstParameter, secondParameter, thirdParameter)
+hints: search google simple interest calculation formula
+ */
